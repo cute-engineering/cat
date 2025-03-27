209b856d2a96e1750b6cdaf0f70482489fff1313
sleepy-monax <me@smnx.sh>
2025-03-27 13:51:42 +0100

    Removed support for syntax highlighting, it didn't work with every themes :(

--- meta/plugins/__init__.py
+++ meta/plugins/__init__.py
@@ -51,7 +51,6 @@ class Site(DataClassJsonMixin):
     favicon: str = dt.field(default="🐱")
     navbar: str = dt.field(default="")
     footer: str = dt.field(default="")
-    code_theme: str = dt.field(default="default")
 
     path: Path = dt.field(default=Path(""))
 
@@ -81,7 +80,7 @@ class Site(DataClassJsonMixin):
             style += "\n\n\n"
             style += readFile(styleFile)
 
-        md = markdown.Markdown(extensions=["codehilite", "meta", "extra"])
+        md = markdown.Markdown(extensions=["meta", "extra"])
         for file in SITE_DIR.rglob("*"):
             if file.is_dir() or file.suffix == ".json":
                 continue
@@ -108,18 +107,6 @@ class Site(DataClassJsonMixin):
                 title = f"{title} - {self.title}"
             else:
                 title = self.title
-
-            style += shell.popen(
-                "pygmentize",
-                "-S",
-                self.code_theme,
-                "-f",
-                "html",
-                "-a",
-                ".codehilite",
-                "-O",
-                "full",
-            )
 
             htmlContent = f"""
 <!DOCTYPE html>
